df0204117b0a2c1f3ca5db52afb23f8ed1abfd99
CraftYun83 <47018858+CraftYun83@users.noreply.github.com>
2020-09-27 16:03:18 +0800

    Survery

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Survery
-Easiest way to create a survey in python!
+# Survery by CraftYun83
+
+This is a simple library to lets you make surveys super easily with just a few commands!.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Survery", # Replace with your own username
+    version="",
+    author="CraftYun83",
+    author_email="craftyun83@gmail.com",
+    description="Easiest way to make a simple survey!",
+    long_description="Survey is the easiest way to make simple surveys!",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages= setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
